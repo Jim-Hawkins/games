315e824912805d0c8e84944596fbeb516e4fa083
Jim-Hawkins <sergiogilnova2001@gmail.com>
2021-06-06 13:54:31 +0200

    comments and little refactor

--- alphabet soup/main.py
+++ alphabet soup/main.py
@@ -18,28 +18,31 @@ class AlphabetSoup:
                 if found: break     # if word was found, do not keep looking
                 for j in range(self.width):
                     if found: break     # if word was found, do not keep looking
-                    if w[0] == self.table[i][j]:
-                        result = self.explore_word(w, i, j)
-                        if result is not None:
-                            print("Palabra {} encontrada entre ({}, {}) y ({}, {}).".format(w, i, j, result[0], result[1]))
-                            found = True
+                    result = self.explore_word(w, i, j)
+                    if result is not None:
+                        print("Palabra {} encontrada entre ({}, {}) y ({}, {}).".format(w, i, j, result[0], result[1]))
+                        found = True
 
             if not found: print("Palabra {} no encontrada.".format(w))
 
     def explore_word(self, word, row, col):
+        """ method to analyze if a word begins in row, col"""
+        # first of all, check if table[row][col] equals our word's first letter
+        if word[0] != self.table[row][col]: return None
+        # get the direction to explore the word
         self.dir = self.get_dir(word, row, col)
-        # print("self.dir ", self.dir)
+        if self.dir is None: return None
         for i in range(1, len(word)):
-            result = self.get_next_index(row, col)
-            if result is None:
-                return result
-            row, col = result
-            # print("row, col, i ", row, col, i)
+            # get the next coords counting from (row, col)
+            row, col = self.get_next_index(row, col)
+            # stop seeking if the ith letter is different from the selected one in the table
+            # or if (row, col) is outside the table
             try:
                 if word[i] != self.table[row][col]:
                     return None
             except IndexError:
                 return None
+        # if we reach the end of the word, return its finish coords
         return row, col
 
     def get_dir(self, word, row, col):
@@ -91,5 +94,5 @@ tablero = [["A", "G", "U", "A"],
            ["P", "I", "R", "T"],
            ["N", "A", "R", "F"],
            ["X", "Q", "S", "E"]]
-palabras = ["AIRE", "AGUA", "TIERRA"]
+palabras = ["AIRE", "AGUA", "TIERRA", "NIU"]
 AlphabetSoup(tablero, palabras).solve()
