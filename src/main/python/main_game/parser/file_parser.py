56f082ff35b0252e35820ec3b9a3a54bd4e12f42
Jim-Hawkins <sergiogilnova2001@gmail.com>
2021-06-08 21:21:42 +0200

    added instructions file and ignoreCase feature in the parser added docstrings

--- src/main/python/main_game/parser/file_parser.py
+++ src/main/python/main_game/parser/file_parser.py
@@ -1,8 +1,11 @@
+""" Parser to adapt and check the input for the game solver"""
+
 from main_game.cfg.games_cfg import GAME_FILES
 from main_game.exceptions.game_exception import GameException
 
 
 class FileParser:
+    """ Class that implements a parser"""
     def __init__(self, game_file):
         self._file = game_file
         self._content = self._parse_file()
@@ -31,9 +34,9 @@ class FileParser:
             if iter >= len(self._content):      # no point was found and iter is out of bounds
                 raise GameException("Incorrect format. Game file corrupted.")
 
-        iter += 1                               # iter is currently in the line of the separator, so we increase it by 1
-        reference = iter                        # reference will be used to check consistency of the board
-        while iter < len(self._content):        # examine board until the end of the list _content
+        iter += 1           # iter is currently in the line of the separator, so we increase it by 1
+        reference = iter                  # reference will be used to check consistency of the board
+        while iter < len(self._content):  # examine board until the end of the list _content
             if len(self._content[iter]) != len(self._content[reference]):
                 raise GameException("Incorrect table format.")
             if not self._content[iter].isalpha():
@@ -46,12 +49,12 @@ class FileParser:
         board = []
         iter = 0
         while self._content[iter] != ".":
-            words.append(self._content[iter])
+            words.append(self._content[iter].upper())
             iter += 1
 
         iter += 1
         while iter < len(self._content):
-            board.append(self._content[iter])
+            board.append(self._content[iter].upper())
             iter += 1
 
         return words, board
